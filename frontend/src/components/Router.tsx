53ee424ba2ceb8dd2f9ac2d753677ebf450d183c
gfrasca <gfrasca@redhat.com>
2022-09-27 00:44:47 -0400

    Update GitHub References in UI to point to ODH Repo Fork

--- frontend/src/components/Router.tsx
+++ frontend/src/components/Router.tsx
@@ -132,9 +132,9 @@ export const RoutePageFactory = {
 };
 
 export const ExternalLinks = {
-  DOCUMENTATION: 'https://www.kubeflow.org/docs/pipelines/sdk/pipelines-with-tekton/',
-  GITHUB: 'https://github.com/kubeflow/kfp-tekton',
-  GITHUB_ISSUE: 'https://github.com/kubeflow/kfp-tekton/issues/new/choose',
+  DOCUMENTATION: 'https://access.redhat.com/documentation/en-us/red_hat_openshift_data_science/1',
+  GITHUB: 'https://github.com/opendatahub-io/ml-pipelines',
+  GITHUB_ISSUE: 'https://github.com/opendatahub-io/ml-pipelines/issues/new/choose',
 };
 
 export interface DialogProps {
